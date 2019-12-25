595d54592da03dbfb82910e59f4e997ccc684f5e
Semen Senkivskyi <semkosenk@gmail.com>
2019-12-25 11:26:29 +0200

    Update task1.js

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,20 +1,20 @@
 //Quadratic equation 
 //ax2 + bx + c = 0
 
-var a = parseInt(prompt("Enter please the value for a:", 0));
-var b = parseInt(prompt("Enter please the value for b:", 0));
-var c = parseInt(prompt("Enter please the value for c:", 0));
+var a = parseInt(prompt("Enter please the value for a:"));
+var b = parseInt(prompt("Enter please the value for b:"));
+var c = parseInt(prompt("Enter please the value for c:"));
 
-if (typeof(a, b, c) === "number") {
+if (typeof (a, b, c) === "number") {
 	var discriminant = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
 
 	if (discriminant > 0) {
-	    var x1 = (-b + discriminant) / (2 * a);
-	    var x2 = (-b - discriminant) / (2 * a);
+	    let x1 = (-b + discriminant) / (2 * a);
+	    let x2 = (-b - discriminant) / (2 * a);
 	    console.log("x1 = " + x1 + "   x2 = " + x2);
 	} 
 	else if (discriminant === 0) {
-	    var x = -b / (2 * a);
+	    let x = -b / (2 * a);
 	    console.log("x = " + x);
 	} 
 	else {
